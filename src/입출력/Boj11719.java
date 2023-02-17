e867a9185f99475b1cb5df70c12a89f81a767663
Eddie <heungryuncho@naver.com>
2023-02-17 14:09:53 +0900

    230217

---
+++ src/입출력/Boj11719.java
@@ -0,0 +1,12 @@
+package 입출력;
+
+import java.util.Scanner;
+
+public class Boj11719 {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()) {
+            System.out.println(sc.nextLine());
+        }
+    }
+}
