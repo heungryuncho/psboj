3898a1dccb2db0bf3785ce542cd5cea2f2dec2fe
Eddie <heungryuncho@naver.com>
2023-02-21 15:36:05 +0900

    230221

---
+++ src/입출력/Boj2739.java
@@ -0,0 +1,15 @@
+package 입출력;
+
+import java.util.Scanner;
+
+public class Boj2739 {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        sc.close();
+
+        for(int i = 1; i<10; i++){
+            System.out.println(N + "*" + i + "=" + (N * i));
+        }
+    }
+}
