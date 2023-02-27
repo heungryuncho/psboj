1cf4caf296b21faeba72903bdba127023038405f
Eddie <heungryuncho@naver.com>
2023-02-27 14:34:13 +0900

    230227

---
+++ src/DP/Boj11726.java
@@ -0,0 +1,17 @@
+package DP;
+
+import java.util.Scanner;
+
+public class Boj11726 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[] dp = new int[N + 2];
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i <= N; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
+        }
+        System.out.println(dp[N]);
+    }
+}
--- src/입출력/Boj10992.java
+++ src/입출력/Boj10992.java
@@ -10,9 +10,23 @@ public class Boj10992 {
 
         for (int i = 1; i <= N; i++) {
             if (i == N) {
-                for(int)
+                for (int k = 1; k <= 2 * i - 1; k++) {
+                    System.out.print("*");
+                }
+                System.out.println();
+                break;
             }
+            for (int j = 1; j <= N - i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= 2 * i - 1; k++) {
+                if (k == 1 || k == 2 * i - 1) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
         }
-
     }
 }
