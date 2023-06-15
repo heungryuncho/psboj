c1fd73669874ee213cdd03856907fc33f99cd61f
Eddie <heungryuncho@naver.com>
2023-06-15 20:19:08 +0900

    update BOJ10816

--- src/Boj10816.java
+++ src/Boj10816.java
@@ -1,29 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
-public class Boj10816 {
-    public static void main(String[] args) {
+public class Boj10816{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        Scanner in = new Scanner(System.in);
-
-        int N = in.nextInt();
+        int N = Integer.parseInt(br.readLine());
         int[] arr = new int[N];
 
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         for (int i = 0; i < N; i++) {
-            arr[i] = in.nextInt();
+            arr[i] = Integer.parseInt(st.nextToken());
         }
+        Arrays.sort(arr);
 
-        Arrays.sort(arr); // 이분 탐색을 하기 위해서는 반드시 정렬이 되어있어야한다.
+        int M = Integer.parseInt(br.readLine());
 
-        int M = in.nextInt();
-
+        st = new StringTokenizer(br.readLine(), " ");
         StringBuilder sb = new StringBuilder();
 
         for (int i = 0; i < M; i++) {
-            int key = in.nextInt();
+            int key = Integer.parseInt(st.nextToken());
 
             // upperBound와 lowerBound의 차이 값을 구한다.
-            sb.append(upperBound(arr,key) - lowerBound(arr, key)).append(' ');
+            sb.append(upperBound(arr, key) - lowerBound(arr, key)).append(' ');
         }
         System.out.println(sb);
     }
@@ -32,22 +35,26 @@ public class Boj10816 {
         int lo = 0;
         int hi = arr.length;
 
-        // lo가 hi랑 같아질 때 까지 반복
-        while (lo < hi ) {
+        // lo가 hi랑 같아질 때까지 반복
+        while (lo < hi) {
             int mid = (lo + hi) / 2; // 중간위치를 구한다.
-            /***
+            /**
              * key 값이 중간 위치의 값보다 작거나 같을 경우
+             *
              * (중복 원소에 대해 왼쪽으로 탐색하도록 상계를 내린다.)
              */
+
             if (key <= arr[mid]) {
                 hi = mid;
             } else {
-                lo = mid +1;
+                lo = mid + 1;
             }
         }
         return lo;
-    }
-    private  static int upperBound(int[] arr, int key){
+
+        }
+
+    private static int upperBound(int[] arr, int key) {
         int lo = 0;
         int hi = arr.length;
 
@@ -58,10 +65,15 @@ public class Boj10816 {
             // key값이 중간 위치의 값보다 작을 경우
             if (key < arr[mid]) {
                 hi = mid;
-            } else { // 중복원소의 경우 else에서 처리된다.
+            } else {
                 lo = mid + 1;
             }
+
         }
         return lo;
     }
+
+
+
+
 }
